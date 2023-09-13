d518317145d80a0039585f8168f1b4e5efa63f1e
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-13 00:48:01 +0000

    if-elif

--- main.py
+++ main.py
@@ -28,9 +28,24 @@ scissors = '''
 #Write your code below this line 👇
 import random
 
-imput_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor\n")
+imput_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor\n"))
 computer_choice = random.randint(0, 2)
 print(f"Computer chose {computer_choice} ")
+
+# if computer_choice > imput_choice:
+#   print("Computer wins!")
+
+# if imput_choice == 0 and computer_choice ==2:
+#   print("User wins!")
+# elif computer_choice > imput_choice:
+#   print("Computer wins!")
+
+if imput_choice == 0 and computer_choice ==2:
+  print("You win!")
+elif computer_choice > imput_choice:
+  print("You lose!")
+else:
+  print("You type an ivalid number, game over")
 
 # imput_choose = int(imput_choose)
 # list_choose = ["Rock", "Paper", "Scissor"]
