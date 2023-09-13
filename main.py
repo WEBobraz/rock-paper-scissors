6c816321254134303952afe80f9954cfd3b2df3a
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-13 00:20:10 +0000

    first

--- main.py
+++ main.py
@@ -27,3 +27,14 @@ scissors = '''
 
 #Write your code below this line 👇
 
+imput_choose = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor ")
+imput_choose = int(imput_choose)
+list_choose = ["Rock", "Paper", "Scissor"]
+if imput_choose == 0:
+    print(rock)
+if imput_choose == 1:
+    print(paper)
+if imput_choose == 2:
+    print(scissors)
+
+
